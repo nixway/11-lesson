b685327cc61dbda4b22f6da7cd6a42d56bc88dc0
Sabir Abdurakhmanov <nihi1ist@mail.ru>
2025-12-24 20:34:17 +0500

    Add pipeline

---
+++ pipeline.groovy
@@ -0,0 +1,42 @@
+pipeline {
+    agent {
+        docker {
+            image 'nihi1ist/builder:0.1-noble'
+            args '-v /var/run/docker.sock:/var/run/docker.sock'
+        }
+    }
+    stages {
+        stage('Checkout') {
+            steps {
+                checkout scmGit(branches: [[name: '*/master']], extensions: [], userRemoteConfigs: [[url: 'https://github.com/nixway/test-war-app.git']])
+            }
+            
+        }
+        stage('Build project') {
+            steps {
+                sh 'mvn clean package'
+            }
+        }
+        stage('Build docker image') {
+            steps {
+                sshagent(credentials: ['test-without-passwd']) {
+                    sh '''ssh-keyscan -H jenkins.nixway.org >> ~/.ssh/known_hosts
+                    scp nihi1ist@jenkins.nixway.org:~/docker-images/testwebpage/Dockerfile ./
+                    docker build -t nihi1ist/mytestwebpage:0.3 .
+                    docker push nihi1ist/mytestwebpage:0.3'''
+                }
+            }
+        }
+        stage('Deploy project in prod') {
+            steps {
+                sshagent(credentials: ['test-without-passwd']) {
+                    sh '''ssh nihi1ist@jenkins.nixway.org << EOF
+                    cd ~/docker-images/testwebpage
+                    docker pull nihi1ist/mytestwebpage:0.3
+                    docker-compose up -d
+                    EOF'''
+                }
+            }
+        }
+    }
+}
